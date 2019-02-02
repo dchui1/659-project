1b6b803db4aa53a287c391e618d092761530818b
Daniel Chui <dchui@coleburn.local>
2019-02-02 01:13:17 -0700

    separate riverswim to separate file

---
+++ riverswim.py
@@ -0,0 +1,55 @@
+class RiverSwim:
+  def __init__(self):
+    self.STEPS_LIMIT = 10000 # number of steps in each episode?
+    self.pos = 0
+    self.swimRightStay = 0.6
+    self.swimRightUp = 0.35
+    self.swimRightDown = 0.05
+    self.S1swimRightUp = 0.6
+    self.SNswimRightDown = 0.4
+
+  def reset(self):
+      self.n = 0
+      self.pos = 0
+      return self.pos
+
+  def step(self, a): # the transition function?
+    old_pos = self.pos
+    # if action is 0 then we do nothing
+    if a == 1:
+      # determine if we will successfully take the "up" action
+      flip = random.random()
+      if self.pos <= 0: # first state in chain
+        if flip > self.S1swimRightUp:
+          self.pos = self.pos + 1
+      elif self.pos >= 5: # end of chain
+        if flip <= self.SNswimRightDown:
+          self.pos = self.pos - 1
+      else: # middle of chain
+        if flip <= self.swimRightDown:
+          self.pos = self.pos - 1
+        elif flip > self.swimRightDown + self.swimRightStay:
+          self.pos = self.pos + 1
+    # make sure that the position we return (the next state) is between 0 and 5
+    self.pos = np.clip(self.pos, 0, 5)
+
+    # tuple indicating (state, reward, terminated, action)
+    # note this is a continuing task, so the environment will never send a
+    # termination signal
+    return (self.pos, self.rewardFunction(old_pos, a), False, a)
+
+  def rewardFunction(self, x, a):
+    if x >= 5 and a == 1:
+      return 1.0
+    if x <= 0 and a == 0:
+      return 5.0/1000.0
+    return 0.0
+
+  def numObservations(self):
+    # position on the river.
+    # states are: 0, 1, 2, 3, 4, 5
+    return 1
+
+  def numActions(self):
+    # (0) stay or (1) swim up the river
+    return 2
