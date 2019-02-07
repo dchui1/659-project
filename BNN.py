f99f8e962042566cf841ce4f365782401d0cee68
Daniel Chui <dchui@coleburn.local>
2019-02-07 00:54:22 -0700

    working on BNN approximator

--- BNN.py
+++ BNN.py
@@ -32,9 +32,15 @@ def new_bnn(log_divergence_weight=-10, prior_stddev=1, residual_weight=0.1):
     Creates a new Bayesian neural network.
 
     Arguments:
-    - log_divergence_weight: The weight of the divergence penalty on each layer. Defaults to -3 since that worked best for me with MNIST.
-    - prior_stddev: The standard deviation of the prior weight distributions. Defaults to 1 since that should probably be a good place to start. Might need to turn this up a lot though to get the layers to be more random, so you could set this as large as 20.
-    - residual_weight: The weight on the residual term in the residual layers. Defaults to 0.1 since that worked best for me. You can set it to zero to make the layers non-residual.
+    - log_divergence_weight: The weight of the divergence penalty on each layer.
+        Defaults to -3 since that worked best for me with MNIST.
+    - prior_stddev: The standard deviation of the prior weight distributions.
+        Defaults to 1 since that should probably be a good place to start.
+        Might need to turn this up a lot though to get the layers to be more
+        random, so you could set this as large as 20.
+    - residual_weight: The weight on the residual term in the residual layers.
+        Defaults to 0.1 since that worked best for me. You can set it to zero
+        to make the layers non-residual.
     '''
     return tf.keras.Sequential([
         tfp.layers.DenseFlipout(
@@ -47,15 +53,19 @@ def new_bnn(log_divergence_weight=-10, prior_stddev=1, residual_weight=0.1):
     ])
 
 #training-data:
-x = np.array([[1, 2, 3], [2, 3, 4]])
-y = np.array([1, 5])
-print(x.shape[0])
+x = np.array([[1., 2., 3.], [2., 3., 4.]], dtype=np.float32)
+y = np.array([1, 5], dtype=np.float32)
+print("Shape of x", x.shape)
 y.shape[0]
 
 # run model
 f = new_bnn()
 f.compile(optimizer=tf.keras.optimizers.Adam(1e-2), loss=tf.losses.huber_loss)
 history = f.fit(x, y, batch_size=32, epochs=1000, verbose=1)#, class_weight=W)
+
+print("Finished compiling")
+print(type(f))
+print(type(x))
 
 # Evaluation:
 prediction_train = f(x)
@@ -67,12 +77,12 @@ for i in range(100):
   predictions_list_trainset.append(y)
 
 predictions_list_trainset = np.asarray(predictions_list_trainset)
-print(predictions_list_trainset.shape)
-print(predictions_list_trainset.mean())
-print((predictions_list_trainset.std(0, ddof=1)).mean())
+print("Predictions shape", predictions_list_trainset.shape)
+print("Predictions mean", predictions_list_trainset.mean())
+print("Predictions std", (predictions_list_trainset.std(0., ddof=1)).mean())
 
 #test-data:
-X_test = np.array([[45, 10, 10], [500, 676, 2000]])
+X_test = tf.convert_to_tensor(np.array([[45., 10., 10.], [500., 676., 2000.]]), dtype=tf.int32)
 
 prediction_test = f(X_test)
 predictions_list_testset = []
--- BNNApproximator.py
+++ BNNApproximator.py
@@ -1,11 +1,14 @@
 import numpy as np
-import BNN
+from BNN import *
 class BNNApproximation(BayesianApproximator):
 
     def __init__(self, state_dimensions, num_acts):
         super().__init__(state_dimensions, num_acts)
-        self.kernel = KernelPrior()
-        self.bnn = self.kernel.new_bnn()
+
+        self.bnn = new_bnn()
+        #TODO refactor this back out into the BNN method
+        f.compile(optimizer=tf.keras.optimizers.Adam(1e-2), loss=tf.losses.huber_loss)
+        history = f.fit(state_dimensions, num_acts, batch_size=32, epochs=1000, verbose=1)
 
 
     def update_stats(self, s, a, val=0.0):
