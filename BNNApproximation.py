80bccdf7f289d940e74d7b2cc3aa2a7571191ac5
Daniel Chui <dchui@coleburn.local>
2019-02-19 20:31:34 -0700

    Change optimizer to sgd

--- BNNApproximation.py
+++ BNNApproximation.py
@@ -9,15 +9,19 @@ class BNNApproximation(BayesianApproximator):
         super().__init__(state_dimensions, num_acts)
         self.session = tf.InteractiveSession()
         self.bnn = new_bnn()
-        self.bnn.compile(optimizer=tf.keras.optimizers.Adam(1e-2),
+        self.bnn.compile(optimizer=tf.keras.optimizers.SGD(1e-1),
             loss=tf.losses.huber_loss)
 
 
-    def update_stats(self, s, a, val=0.0, batch_size = 32, epochs = 100):
+    def update_stats(self, s, a, val=0.0, batch_size = 1, epochs = 10):
         if self.dimensions.shape != s.shape or not a <= self.num_actions:
             raise ValueError("Invalid value to update stats", s, a, val)
 
-        input_vector = np.concatenate((s, self.convert_action(a)))
+        # print("State action", s, a)
+        input_vector = np.concatenate((self.convert_state(s), self.convert_action(a)))
+        # print("The input vector shape", input_vector.shape)
+        # print("The input vector", input_vector)
+
 
         self.bnn.fit(np.array([[input_vector]]), np.array([[val]]), batch_size=batch_size, epochs=epochs, verbose=0)
 
@@ -26,14 +30,20 @@ class BNNApproximation(BayesianApproximator):
         if self.dimensions.shape != s.shape or not a <= self.num_actions:
             raise ValueError("Invalid value to sample", s, a)
 
-        input_vector = np.concatenate((s, self.convert_action(a)))
+        input_vector = np.concatenate((self.convert_state(s), self.convert_action(a)))
 
-        # with self.session as sess:
-            # sess.run(tf.global_variables_initializer())
-            # sess.run(tf.tables_initializer())
         sample_fn = self.bnn(np.array([[input_vector]], dtype=np.float32))
         predictions = [self.session.run(sample_fn) for i in range(n)]
         return predictions
+
+    def convert_state(self, s):
+        vectors = []
+        for i in range(len(self.dimensions)):
+            arr = np.zeros(self.dimensions[i])
+
+            arr[s[i]] = 1
+            vectors.append(arr)
+        return np.concatenate(vectors)
 
     def convert_action(self, a):
         arr = np.zeros(self.num_actions)
