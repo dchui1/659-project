6b69b8fc26c56da9c0670f4d63eb749414a01041
andy <andnpatterson@gmail.com>
2019-02-23 13:01:53 -0700

    setup parameter sweep scripts

---
+++ scripts/find_best.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+from ExperimentDescription import ExperimentDescription
+
+def parse_args():
+  parser = argparse.ArgumentParser("Reinforcement Learning experiments for multiagent environments")
+  parser.add_argument("-e", type=str, help="path to experiment description json file")
+  parser.add_argument("-b", type=str, default='results', help="base path for saving results")
+
+  args = parser.parse_args()
+  if args.b == None:
+    print('Please run again using (without angle braces):')
+    print('python -m scripts.find_best.py -e path/to/exp.json')
+    exit(1)
+
+  return args
+
+args = parse_args()
+exp = ExperimentDescription(args.e, 0)
+
+base = f'{args.b}/{exp.name}/{exp.environment}/{exp.agent}'
+param_results = os.listdir(base)
+
+def getResults(path):
+    with open(f'{base}/{path}/mean.csv', 'r') as f:
+        return { 'path': path, 'mean': float(f.read()) }
+
+results = map(getResults, param_results)
+min_result = min(results, key = lambda x: x['mean'])
+
+print(min_result)
---
+++ scripts/run_sweep_local.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+import subprocess
+from ExperimentDescription import ExperimentDescription
+
+cpus = 8
+
+def parse_args():
+  parser = argparse.ArgumentParser("Reinforcement Learning experiments for multiagent environments")
+  parser.add_argument("-e", type=str, help="path to experiment description json file")
+  parser.add_argument("-r", type=int, help="number of runs to complete")
+  parser.add_argument("-b", type=str, default='results', help="base path for saving results")
+
+  args = parser.parse_args()
+  if args.b == None or args.r == None:
+    print('Please run again using (without angle braces):')
+    print('python -m scripts.run_sweep_local -e path/to/exp.json -r <num>')
+    exit(1)
+
+  return args
+
+args = parse_args()
+exp = ExperimentDescription(args.e, 0, args.r)
+num = exp.num_permutations
+
+runs = '{0..' + str(num-1) + '}'
+
+parallel = f'parallel -j{cpus} python parameter_sweep.py -e {args.e} -r {args.r} -b {args.b} -i ::: {runs}'
+print(parallel)
+process = subprocess.run(parallel, stdout=subprocess.PIPE, shell=True, executable='/bin/zsh')
