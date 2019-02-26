2123a8ef3b955524091806ae4d87b0649dc961eb
Daniel Chui <dchui@coleburn.local>
2019-02-26 13:24:31 -0700

    Add plot code

---
+++ plot.py
@@ -0,0 +1,32 @@
+
+import matplotlib.pyplot as plt
+import pickle
+import sys
+
+def main():
+    fileName =sys.argv[1]
+    print(fileName)
+    f1 = open(fileName, 'rb')
+    data_dict = pickle.load(f1)
+    (rewards, stderr) = data_dict["results"]
+    fig = plt.figure()
+    ax = plt.axes()
+    plotRewards(ax, rewards, stderr, fileName)
+
+    plt.legend()
+    plt.title("Average Number of Steps to Reach Goal across 5 Runs")
+    plt.xlabel("Number of Episodes")
+    plt.ylabel("Average Number of Steps to Reach Goal")
+    plt.show()
+
+
+def plotRewards(ax, rewards, stderr, label):
+  (low_ci, high_ci) = confidenceInterval(rewards, stderr)
+  ax.plot(rewards, label=label)
+  ax.fill_between(range(rewards.shape[0]), low_ci, high_ci, alpha=0.4)
+
+def confidenceInterval(mean, stderr):
+  return (mean - stderr, mean + stderr)
+
+if __name__ == "__main__":
+    main()
