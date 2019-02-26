7ec67b3b2df35717c3b8bfa67da0f6194c69edcc
Fatima Davelouis <daveloui@ualberta.ca>
2019-02-26 10:42:38 -0700

    .json file to run the LinearQ-TDistR agent, and importing tensorflow for the linear_model (we don't use it right now, but might come handy in the future).

--- tf_supervised_inference/linear_model.py
+++ tf_supervised_inference/linear_model.py
@@ -1,9 +1,14 @@
+import tensorflow as tf
+import numpy as np
+
 class LinearModel(object):
     def __init__(self, weights):
         self.weights = [weights]
+        print(weights.shape)
 
     def __call__(self, phi):
         return phi @ self.weights[0]
 
     def predict(self, phi):
+        phi = tf.convert_to_tensor(phi, dtype=np.float32)
         return self(phi)
