d4062a9bd3f55261c2bf455ee5ab5314e2df298d
Daniel Chui <dchui@coleburn.local>
2019-02-26 17:52:33 -0700

    have plot accept multiple files

--- plot.py
+++ plot.py
@@ -4,14 +4,20 @@ import pickle
 import sys
 
 def main():
-    fileName =sys.argv[1]
-    print(fileName)
-    f1 = open(fileName, 'rb')
-    data_dict = pickle.load(f1)
-    (rewards, stderr) = data_dict["results"]
+    # fileName =sys.argv[1]
+
     fig = plt.figure()
     ax = plt.axes()
-    plotRewards(ax, rewards, stderr, fileName)
+    # plotRewards(ax, rewards, stderr, fileName)
+
+    for i in range(1, len(sys.argv)):
+        fileName = sys.argv[i]
+        f1 = open(fileName, 'rb')
+        data_dict = pickle.load(f1)
+        (rewards, stderr) = data_dict["results"]
+        print(sys.argv[i])
+        plotRewards(ax, rewards, stderr, fileName)
+
 
     plt.legend()
     plt.title("Average Number of Steps to Reach Goal across 5 Runs")
