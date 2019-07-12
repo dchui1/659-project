2c12b175eadf535a00a9c1b829c1c13a8f1bcab5
Parash Rahman <parash@ualberta.ca>
2019-07-12 16:28:29 -0600

    poster work

--- src/agents/PosterBayesianQLearningTabular.py
+++ src/agents/PosterBayesianQLearningTabular.py
@@ -8,8 +8,9 @@ from src.agents.TabularQ import TabularQ
 from src.bayesian_inference.TabularQApproximation import TabularQApproximation
 from src.utils.math import argMax
 
-class BayesianQLearningTabular(TabularQ):
+class PosterBayesianQLearningTabular(TabularQ):
     def __init__(self, state_shape, num_acts, params):
+        print(params)
         super().__init__(state_shape, num_acts, params)
         self.gamma = params['gamma']
         self.bayesianQ = TabularQApproximation(state_shape, num_acts, self.gamma, params)
@@ -25,6 +26,6 @@ class BayesianQLearningTabular(TabularQ):
         # we sample 100 for each
         self.act_vals = [self.bayesianQ.sample(self.getIndex(s) + (a * self.num_states), 100) for a in range(self.num_acts)]
         # get the nth percentie
-        self.act_vals = np.sort(self.act_vals, axis=1)[:,self.percentile]
+        self.act_vals = np.sort(self.act_vals, axis=1)[:,self.percentile-1]
         move = argMax(self.act_vals)
         return move
